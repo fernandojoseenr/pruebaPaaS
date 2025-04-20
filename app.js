6e7848e8d1b872c963ba2e9b9efc8477d2210182
fernandojoseenr <sanchesdiego912@gmail.com>
2025-04-19 21:08:12 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,10 +1,31 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
-app.get('/', (req, res) => {
-    res.send('Hola estamos en senati desplegando nuestro PaaS 2025');
+
+
+
+cons cosmos de Cosos DB
+const cosmosCliente=new CosmosClient(process.env.COSMOS_BD_CONNECTION_STRNG);
+const database=cosmosClient.database('academic-db');
+const container-database.container('student');
+app.use(express.json());
+
+
+app.post('/student',async(req,res)=>(
+    try{
+        const(body)=req;
+        const(resource:createdItem)=await container.items.create(body);
+        res.status(201).send(createdItem);
+        }catch(error){
+        res.status(500).send(error;
+        }
 });
 
-app.listen(port, () => {
-    console.log('Aplcacion escuchando en el puerto $(port)');
-})
+app.get('students',async(req,res)=>(
+    try{
+        const(resources)=await container.items.readAll().fetchAll();
+        res.status(200).send(resources);
+}catch(error){
+    res.status(500).send(error);
+
+});
